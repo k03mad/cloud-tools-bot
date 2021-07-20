8a35db725ddd52d9a862bfca2938bd75d30aa018
k03mad <k03.mad@gmail.com>
2021-07-20 18:34:32 +0300

    update f2b

--- app/cmd/f2b/status.js
+++ app/cmd/f2b/status.js
@@ -4,9 +4,11 @@ const {shell} = require('@k03mad/utils');
 
 /** @returns {Promise<string>} */
 module.exports = async () => {
+    const jails = ['sshd', 'grafana'];
+
     const data = [
         await shell.run('sudo fail2ban-client status'),
-        await shell.run('sudo fail2ban-client status sshd'),
+        ...await Promise.all(jails.map(jail => shell.run(`sudo fail2ban-client status ${jail}`))),
     ];
 
     return data.join('\n\n');
