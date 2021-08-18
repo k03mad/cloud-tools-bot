4a2a70846c3d604a0ff817df4603a2b0bc130ac6
Kirill Molchanov <k03.mad@gmail.com>
2021-08-18 23:58:24 +0300

    Create update.js

---
+++ app/cmd/cargo/update.js
@@ -0,0 +1,10 @@
+'use strict';
+ 
+const {shell} = require('@k03mad/utils');
+ 
+/** @returns {Promise<string>} */
+module.exports = () => shell.run([
+    'rustup update',
+    'cargo install-update -a',
+]);
+ 
