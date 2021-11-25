43e713f489bcfb038f9e893b39350e9c9c4f45fd
k03mad <k03.mad@gmail.com>
2021-11-25 22:55:05 +0300

    use webhook

--- env.js
+++ env.js
@@ -4,5 +4,13 @@ module.exports = {
     telegram: {
         chat: Number(process.env.TELEGRAM_MY_CHAT),
         token: process.env.TELEGRAM_CLOUD_TOKEN,
+        port: process.env.TELEGRAM_PORT,
+    },
+    cloud: {
+        is: process.env.IS_CLOUD,
+        domain: process.env.CLOUD_DOMAIN,
+    },
+    cert: {
+        path: process.env.CERT_PATH,
     },
 };
