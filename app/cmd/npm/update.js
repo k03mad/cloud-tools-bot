63febee9b7f8f97db65e4c8c49ecc358990bffa3
k03mad <k03.mad@gmail.com>
2022-02-09 13:27:33 +0300

    update deps

--- app/cmd/npm/update.js
+++ app/cmd/npm/update.js
@@ -13,6 +13,7 @@ export default async () => {
     const logs = ['__ NPM __'];
 
     const outdated = await shell.run('npm -g outdated --parseable --depth=0', {returnOnErr: true});
+
     const parsed = outdated
         .split(/\s+/)
         .map(elem => elem.split(':')[3])
